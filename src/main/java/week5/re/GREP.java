67c0c053030810d46a477f3cb848de371a66f6aa
matijavizintin <matija.vizintin@gmail.com>
2017-02-11 15:39:14 +0100

    grep impl

---
+++ src/main/java/week5/re/GREP.java
@@ -0,0 +1,22 @@
+package week5.re;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by matijav on 11/02/2017.
+ */
+public class GREP {
+
+    public static void main(String[] args) {
+        String regex = "(.*" + args[0] + ".*)";
+        NFA nfa = new NFA(regex);
+
+        while (StdIn.hasNextLine()) {
+            String line = StdIn.readLine();
+            if (nfa.recognizes(line)) {
+                StdOut.println(line);
+            }
+        }
+    }
+}
---
+++ src/main/java/week5/re/Harvester.java
@@ -0,0 +1,27 @@
+package week5.re;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by matijav on 11/02/2017.
+ */
+public class Harvester {
+
+    public static void main(String[] args) {
+        String regex = args[0];
+//        In in = new In(args[1]);
+//        String input = in.readAll();
+        String input = args[1];
+        input = input.replaceAll("_", " ");
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            StdOut.println(matcher.group());
+        }
+    }
+}
